4cd65a1c90d2b5a8216be2d69ec62f49f6e4b805
Yurii Cherkasov <strategarius@protonmail.com>
2024-03-11 14:12:55 +0700

    Rollback

--- database_widget.py
+++ database_widget.py
@@ -1,6 +1,5 @@
 import os
 
-from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QTableView, QDialog
 from PyQt5.QtWidgets import QLabel, QLineEdit, QDialogButtonBox
 from PyQt5.QtSql import QSqlDatabase, QSqlQuery, QSqlTableModel
@@ -15,7 +14,6 @@ class DatabaseWidget(QWidget):
                            "(ID INTEGER PRIMARY KEY AUTOINCREMENT, Timestamp TEXT, Event TEXT)"
     INSERT_CPU_WORKLOAD = "INSERT INTO CpuWorkload (Timestamp, PID, ProcessName, Workload) " \
                           "VALUES (:timestamp, :pid, :process_name, :workload)"
-    DATABASE_FILE = "CpuMetrics"
 
     def __init__(self, rewrite_database):
         super().__init__()
@@ -29,7 +27,6 @@ class DatabaseWidget(QWidget):
         self.cpu_workload_model = QSqlTableModel()
         self.system_events_model = QSqlTableModel()
         self.init_ui()
-        self.setup_database()
 
     def init_ui(self):
         self.layout = QVBoxLayout(self)
@@ -37,12 +34,8 @@ class DatabaseWidget(QWidget):
 
         # Horizontal panel for buttons
         self.button_panel_layout = QHBoxLayout()
-        self.create_button("Create", self.create_database)
-        self.create_button("Import", None)  # Replace with None
-        self.create_button("Export", None)  # Replace with None
-        self.create_button("Cleanup", None)  # Replace with None
-        self.create_button("Drop", None)  # Replace with None
-        self.create_button("Backup", None)  # Replace with None
+        self.create_button("Create", self.create_db)
+        self.create_button("Insert", self.test_insert)
         self.layout.addLayout(self.button_panel_layout)
 
         # Table views for CpuWorkload and SystemEvents
@@ -52,84 +45,57 @@ class DatabaseWidget(QWidget):
         self.models_layout.addWidget(self.system_events_table)
         self.layout.addLayout(self.models_layout)
 
+        # Set up the database models
+        self.cpu_workload_model.setTable("CpuWorkload")
+        self.cpu_workload_model.select()
+        self.system_events_model.setTable("SystemEvents")
+        self.system_events_model.select()
+
+        # Set models for table views
+        self.cpu_workload_table.setModel(self.cpu_workload_model)
+        self.system_events_table.setModel(self.system_events_model)
+
         # Adjust column widths
         for view in [self.cpu_workload_table, self.system_events_table]:
             header = view.horizontalHeader()
             header.setSectionResizeMode(QHeaderView.ResizeToContents)
 
-    def setup_database(self):
-        """
-        Set up the database, either by loading it from the file or creating a new one
-        """
-        if os.path.exists(self.DATABASE_FILE):
-            self.db = QSqlDatabase.addDatabase("QSQLITE")
-            self.db.setDatabaseName(self.DATABASE_FILE)
-            if not self.db.open():
-                print("Failed to open database!")
-                return
-            print("Database opened successfully!")
-
-            # Set models for table views
-            self.cpu_workload_model.setTable("CpuWorkload")
-            self.system_events_model.setTable("SystemEvents")
-            self.cpu_workload_model.select()
-            self.system_events_model.select()
-
-            # Ensure that model data is fetched
-            self.cpu_workload_model.fetchAll()
-            self.system_events_model.fetchAll()
-
-            self.cpu_workload_table.setModel(self.cpu_workload_model)
-            self.system_events_table.setModel(self.system_events_model)
-
-            # Set headers
-            self.set_table_headers(self.cpu_workload_model, self.cpu_workload_table)
-            self.set_table_headers(self.system_events_model, self.system_events_table)
-        else:
-            print("Database file not found, creating new database...")
-
-    def set_table_headers(self, model, table_view):
-        """
-        Set table headers based on the model
-        """
-        header = table_view.horizontalHeader()
-        header.setSectionResizeMode(QHeaderView.ResizeToContents)
-        for i in range(model.columnCount()):
-            header.setSectionResizeMode(i, QHeaderView.ResizeToContents)
-            header.setSectionResizeMode(i, QHeaderView.Interactive)
-            header.setStretchLastSection(False)
-            header.setSortIndicatorShown(True)
-            header.setSortIndicator(i, Qt.AscendingOrder)
-            header.setTextAlignment(Qt.AlignLeft | Qt.AlignVCenter)
-            header.setSectionHidden(i, False)
-
     def create_button(self, name, callback):
         button = QPushButton(name)
         button.setFixedWidth(100)
+        # noinspection PyUnresolvedReferences
+        button.clicked.connect(callback)
         self.button_panel_layout.addWidget(button)
-        if callback:
-            button.clicked.connect(callback)
 
-    def create_database(self):
-        if os.path.exists(self.DATABASE_FILE) and not self.rewrite_database:
-            self.setup_database()
-            return
+    def create_db(self):
+        name = "CpuMetrics"
+        if os.path.exists(name) and self.rewrite_database is False:
+            self.db = QSqlDatabase.addDatabase("QSQLITE")
+            self.db.setDatabaseName(name)
+            if not self.db.open():
+                print("Failed to open database!")
+                return
+            else:
+                print("Database opened successfully!")
+                # Setup table models after database open
+                self.setup_table_models()
+                return
 
         dialog = QDialog(self)
         dialog.setWindowTitle("Create Database")
         layout = QVBoxLayout()
 
         label = QLabel("Database Name:")
-        edit = QLineEdit(self.DATABASE_FILE)
+        edit = QLineEdit(name)
         buttons = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
 
         def create():
             db_name = edit.text()
-            if os.path.exists(db_name) and not self.rewrite_database:
+            if os.path.exists(db_name) and self.rewrite_database is False:
                 print("Database file already exists, exiting")
                 dialog.reject()
                 return
-            elif os.path.exists(db_name) and self.rewrite_database:
+            elif os.path.exists(db_name) and self.rewrite_database is True:
                 print("Database file already exists, delete")
                 os.remove(db_name)
 
@@ -140,13 +106,13 @@ class DatabaseWidget(QWidget):
             else:
                 print("Database created successfully!")
                 self.create_tables()
-                self.setup_database()
+                self.setup_table_models()  # Setup table models after database creation
                 dialog.accept()
 
+        # noinspection PyUnresolvedReferences
         buttons.accepted.connect(create)
+        # noinspection PyUnresolvedReferences
         buttons.rejected.connect(dialog.reject)
-
-        self.setup_database()
 
         layout.addWidget(label)
         layout.addWidget(edit)
@@ -154,6 +120,33 @@ class DatabaseWidget(QWidget):
 
         dialog.setLayout(layout)
         dialog.exec_()
+
+    def setup_table_models(self):
+        # Set up table models
+        self.cpu_workload_model = QSqlTableModel()
+        self.system_events_model = QSqlTableModel()
+        self.cpu_workload_model.setTable("CpuWorkload")
+        self.system_events_model.setTable("SystemEvents")
+        self.cpu_workload_model.select()
+        self.system_events_model.select()
+
+        # Set models for table views
+        self.cpu_workload_table.setModel(self.cpu_workload_model)
+        self.system_events_table.setModel(self.system_events_model)
+
+        # Get the width of each TableView
+        cpu_workload_width = self.cpu_workload_table.width()
+        system_events_width = self.system_events_table.width()
+
+        # Calculate the column width for CPU Workload Table
+        cpu_workload_column_width = cpu_workload_width / self.cpu_workload_model.columnCount()
+        for col in range(self.cpu_workload_model.columnCount()):
+            self.cpu_workload_table.setColumnWidth(col, cpu_workload_column_width)
+
+        # Calculate the column width for System Events Table
+        system_events_column_width = system_events_width / self.system_events_model.columnCount()
+        for col in range(self.system_events_model.columnCount()):
+            self.system_events_table.setColumnWidth(col, system_events_column_width)
 
     def create_tables(self):
         query = QSqlQuery()
@@ -164,20 +157,34 @@ class DatabaseWidget(QWidget):
         else:
             print("Tables created successfully!")
 
-    def insert_cpu_workload(self, cpu_workload):
-        timestamp = cpu_workload['timestamp']
-        pid = cpu_workload['pid']
-        process_name = cpu_workload['process_name']
-        workload = cpu_workload['workload']
+    def insert_metric(self, cpu_usage_history):
+        """
+        Inserts a new metric record into the CpuWorkload table
+        :param cpu_usage_history: dictionary containing CPU usage data
+        """
+        # Prepare the query to insert the record
+        cpu_usage = cpu_usage_history[-1]
         query = QSqlQuery()
-        query.prepare(self.INSERT_CPU_WORKLOAD)
-        query.bindValue(":timestamp", timestamp)
-        query.bindValue(":pid", pid)
-        query.bindValue(":process_name", process_name)
-        query.bindValue(":workload", workload)
+        query.prepare(
+            "INSERT INTO CpuWorkload (Timestamp, PID, ProcessName, Workload) "
+            "VALUES (:timestamp, :pid, :process_name, :workload)"
+        )
+        query.bindValue(":timestamp", cpu_usage['timestamp'])
+        query.bindValue(":pid", cpu_usage['pid'])
+        query.bindValue(":process_name", cpu_usage['process_name'])
+        query.bindValue(":workload", cpu_usage['workload'])
 
+        # Execute the query
         if not query.exec_():
             print("Failed to insert metric:", query.lastError().text())
         else:
             print("Metric inserted successfully!")
+
+            # Refresh the model to update the view with the new data
             self.cpu_workload_model.select()
+
+    def test_insert(self):
+        test_cpu_payload = [
+            {"timestamp": 1, "pid": 1, "process_name": "test1", "workload": 1.1},
+        ]
+        self.insert_cpu_workload(test_cpu_payload)
